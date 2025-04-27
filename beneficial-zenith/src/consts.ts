501d428d164199fb5683da8f78e9d261c5c37dc5
Justin Tung <Jtung1104@gmail.com>
2025-04-27 11:37:00 -0700

    update styles

--- beneficial-zenith/src/consts.ts
+++ beneficial-zenith/src/consts.ts
@@ -5,18 +5,18 @@ export const SITE_URL = 'https://bigbussy.com';
 export const SOCIAL_LINKS = [
   {
     icon: 'youtube',
-    url: 'https://youtube.com/@bigbussy',
+    url: 'https://youtube.com/@2_dolla_heaux',
   },
   {
     icon: 'twitter',
-    url: 'https://twitter.com/bigbussy',
+    url: 'https://twitter.com/2_dolla_heaux',
   },
   {
     icon: 'instagram',
-    url: 'https://instagram.com/bigbussy',
+    url: 'https://instagram.com/2_dolla_heaux',
   },
   {
     icon: 'tiktok',
-    url: 'https://tiktok.com/@bigbussy',
+    url: 'https://tiktok.com/@2_dolla_heaux',
   },
 ]; 
